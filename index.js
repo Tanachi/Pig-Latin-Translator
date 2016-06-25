f947a754d6978b1f2dcbe711c26be49520083fd1
Tanachi <tanachige@gmail.com>
2016-06-25 11:22:52 -1000

    Added period fixing

--- index.js
+++ index.js
@@ -7,17 +7,23 @@ function translator(phrase){
   var words = '';
   var isPig = 0;
   if(typeof phrase === typeof ''){
+    // Split sentances into array of words
     newString = phrase.split(' ');
+    // check the first word to see if it's in pig langauge
     if(newString[0].charAt(newString[0].length - 2) + newString[0].charAt(newString[0].length - 1) === 'ay'){
       isPig = 1;
     }
+    // translates each word into the other langauge
     newString.forEach(function(element,index,array){
+      // translates from pig to english
       if(isPig === 1){
         var pigSplit = element.split('-');
         var ayyyy = pigSplit[1].split('ay');
         words += (ayyyy[0] + pigSplit[0] + ' ');
       }
+      // translates from english to pig
       else{
+        // checks to see if a word starts with a vowel
         if(element.charAt(0) === 'i'  || element.charAt(0) === 'I' ||
            element.charAt(0) === 'a'  || element.charAt(0) === 'A' ||
            element.charAt(0) === 'e'  || element.charAt(0) === 'E' ||
@@ -25,6 +31,7 @@ function translator(phrase){
            element.charAt(0) === 'u'  || element.charAt(0) === 'U'){
             element += '-ay';
         }
+        // checks for the first vowel in each word.
         else{
           var letterSplit = element.split('');
           var position = 0;
@@ -42,14 +49,22 @@ function translator(phrase){
               vowelIndex++;
             }
           }
+          // splits word from first vowel
           var spliceLetters = element.slice(0, vowelIndex);
           var otherLetters = element.slice(vowelIndex,element.length);
-          element =otherLetters + '-' + spliceLetters + 'ay';
+          // translates word into pig
+          element = otherLetters + '-' + spliceLetters + 'ay';
+          // puncuation for finish sentances
+          var dotSplit = element.split('.');
+          if(dotSplit.length > 1)
+            element = dotSplit[0] + dotSplit[1] + '.';
         }
+        // add new word into sentance
         words += element + ' ';
       }
     });
   }
-  console.log(words);
+  console.log(words + '\n');
 }
+translator(quote);
 translator(quote2);
